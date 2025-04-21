0e6ae1b84bfd34f4488f9de29add91d176ae195c
Andrey <strocksmin@yandex.ru>
2025-04-21 18:38:15 +0300

    SGSR: add calculator v.1.5.3

--- src/test/java/org/example/CalculatorTest.java
+++ src/test/java/org/example/CalculatorTest.java
@@ -1,9 +1,8 @@
 package org.example;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
-import static org.junit.Assert.*;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class CalculatorTest {
 
